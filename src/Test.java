f26d31513e967661c51c4ab4f571a08d156beca9
FURKAN ŞENER <furkansenerofficial@gmail.com>
2022-11-23 16:11:34 +0300

    current

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,17 @@
+import javafx.scene.shape.Circle;
+
+import java.util.Scanner;
+
 public class Test {
-    
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter");
+        int i = input.nextInt();
+        String s = input.nextLine();
+
+        System.out.println(i);
+        System.out.println(s);
+
+    }
 }
